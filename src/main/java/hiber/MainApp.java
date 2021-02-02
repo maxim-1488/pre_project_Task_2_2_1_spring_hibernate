3232eb0f7ad28678f49ba886e0295781e2554f22
Maxim <m.krylov88@mail.ru>
2021-02-02 23:54:03 +0300

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,29 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Lada", 2108)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Kia", 11)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("BMW", 777)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Hyundai", 35)));
+
+      List<User> users = userService.listUsers();
+      users.forEach(System.out::println);
+
+      context.close();
+   }
+}
